276c310665a52794461a9edafcdc399f5a79d459
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-05-13 16:43:31 +0200

    react

--- src/components/Expenses/ExpanseItem.js
+++ src/components/Expenses/ExpanseItem.js
@@ -1,33 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpanseDate from "./ExpanseDate";
 import Card from "../UI/Card";
 import "./ExpanseItem.css";
 
 function ExpanseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  let changeBoolean = true;
-  const titleHandler = () => {
-    if (changeBoolean) {
-      setTitle("Hidden!!!");
-      changeBoolean = false;
-    } else {
-      setTitle(props.title);
-      changeBoolean = true;
-    }
+  // const [title, setTitle] = useState(props.title);
+  // let changeBoolean = true;
+  // const titleHandler = () => {
+  //   if (changeBoolean) {
+  //     setTitle("Hidden!!!");
+  //     changeBoolean = false;
+  //   } else {
+  //     setTitle(props.title);
+  //     changeBoolean = true;
+  //   }
 
-    // propsTitle = "Hello world";
-    console.log("ahoj");
-  };
+  // propsTitle = "Hello world";
 
   return (
     <Card className="expense-item">
       <ExpanseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
 
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={titleHandler}>Click</button>
+      {/* <button onClick={titleHandler}>Click</button> */}
     </Card>
   );
 }
